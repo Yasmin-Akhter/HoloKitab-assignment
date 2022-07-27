2834143336e4780016323adfbc57636950410942
Yasmin Akhter <yasminbd89@yahoo.com>
2022-07-28 01:45:04 +0600

    updated icon part

--- src/Components/Icon.js
+++ src/Components/Icon.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faBookOpen } from '@fortawesome/free-solid-svg-icons';
 import { faMagicWandSparkles } from '@fortawesome/free-solid-svg-icons';
+import { faWaveSquare } from '@fortawesome/free-solid-svg-icons';
 
 
 
 const Icon = () => {
     return (
-        <div>
-            <h1>Icon</h1>
-            <FontAwesomeIcon icon={faCoffee} />
-            <FontAwesomeIcon className='text-yellow-400' icon={faMagicWandSparkles} />
+
+
+        <div className='grid grid-rows-3 gap-10 lg:ml-24 ml-6  ' >
+            <div className='grid grid-cols-3'>
+                <p><FontAwesomeIcon className='text-red-400 pr-2 ' icon={faBookOpen} />open books</p>
+                <p><FontAwesomeIcon className='text-yellow-400' icon={faMagicWandSparkles} /> magical animation</p>
+                <p><FontAwesomeIcon className='text-blue-400' icon={faWaveSquare} /> interactive sound </p>
+
+            </div>
+            <div className='grid grid-cols-3'>
+                <p><FontAwesomeIcon className='text-red-400 pr-2' icon={faBookOpen} />open books</p>
+                <p><FontAwesomeIcon className='text-yellow-400' icon={faMagicWandSparkles} /> magical animation</p>
+                <p><FontAwesomeIcon className='text-blue-400' icon={faWaveSquare} /> interactive sound </p>
+            </div>
+            <div className='grid grid-cols-3'>
+                <p><FontAwesomeIcon className='text-red-400 pr-2' icon={faBookOpen} />open books</p>
+                <p><FontAwesomeIcon className='text-yellow-400' icon={faMagicWandSparkles} /> magical animation</p>
+                <p><FontAwesomeIcon className='text-blue-400' icon={faWaveSquare} /> interactive sound </p>
+            </div>
 
         </div>
+
+
+
+
     );
 };
 
